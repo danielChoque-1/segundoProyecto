f40b4107828323be67c61297f4a499bb820a27c8
danielChoque-1 <choquev8@gmail.com>
2023-06-09 22:19:49 -0300

    Repaso clase api

--- src/components/AxiosGet.jsx
+++ src/components/AxiosGet.jsx
@@ -30,24 +30,3 @@ const AxiosGet = () => {
 
 export default AxiosGet
 
-
-// import axios from 'axios'
-// import { useState, useEffect } from 'react'
-
-// const AxiosGet = () => {
-//     const [gatito, setGatito] = useState()
-//     const url = 'https://api.thecatapi.com/v1/images/search'
-
-//     useEffect(() => {
-//         axios(url).then((res) => setGatito(res.data))
-//     }, [])
-    
-//     // console.log(gatito[0]?.url)
-//   return (
-//     <div>
-//        {gatito &&  <img src={gatito[0]?.url} alt="" />}
-//     </div>
-//   )
-// }
-
-// export default AxiosGet
